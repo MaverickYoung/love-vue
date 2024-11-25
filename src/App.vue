011feac2af0c90969ab13d91547295319f6b92a4
Fmy0727 <2410345576@qq.com>
2024-11-26 02:13:55 +0800

    :bug: 修复登录界面显示标签的bug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <router-view/>
 
-  <van-tabbar route>
+  <!-- 判断当前路由是否为登录页面，不是登录页时显示 tabbar -->
+  <van-tabbar v-if="!isLoginPage" route>
     <van-tabbar-item replace to="/poop" icon="home-o">便便</van-tabbar-item>
     <van-tabbar-item replace to="/setting" icon="setting-o">设置</van-tabbar-item>
   </van-tabbar>
 </template>
+
+<script setup>
+import {useRoute} from 'vue-router'
+import {computed} from "vue";
+
+const route = useRoute()
+const isLoginPage = computed(() => {
+  return route.name === 'login'
+})
+</script>
