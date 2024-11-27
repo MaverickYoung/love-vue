c6c0fbdf324beb78ef31901df7e1d7792a2f565b
Maverick <2410345576@qq.com>
2024-11-27 16:29:09 +0800

    :poop: 主题持久化

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,7 +1,8 @@
 import {defineStore} from 'pinia';
+import cache from "@/utlis/cache";
 
 // 主题接口，定义所有可配置的 CSS 变量
-interface Theme {
+export interface Theme {
     '--van-primary-color': string;
     '--van-success-color': string;
     '--van-danger-color': string;
@@ -9,51 +10,48 @@ interface Theme {
     '--van-text-color': string;
     '--van-text-color-2': string;
     '--van-text-color-3': string;
-    '--van-active-color': string;
-    '--van-active-opacity': number;
-    '--van-disabled-opacity': number;
     '--van-background': string;
     '--van-background-2': string;
     '--van-background-3': string;
     '--box-shadow': string;
 }
 
+export const lightTheme: Theme = {
+    '--van-primary-color': '#1989fa',
+    '--van-success-color': '#07c160',
+    '--van-danger-color': '#ee0a24',
+    '--van-warning-color': '#ff976a',
+    '--van-text-color': '#323233',
+    '--van-text-color-2': '#969799',
+    '--van-text-color-3': '#c8c9cc',
+    '--van-background': '#f7f8fa',
+    '--van-background-2': '#fff',
+    '--van-background-3': '#f1f1f1',
+    '--box-shadow': 'rgba(0, 0, 0, 0.1)',
+}
+
+const darkTheme: Theme = {
+    '--van-primary-color': '#1989fa',
+    '--van-success-color': '#07c160',
+    '--van-danger-color': '#ee0a24',
+    '--van-warning-color': '#ff976a',
+    '--van-text-color': '#f5f5f5',
+    '--van-text-color-2': '#707070',
+    '--van-text-color-3': '#4d4d4d',
+    '--van-background': '#000',
+    '--van-background-2': '#1c1c1e',
+    '--van-background-3': '#37363b',
+    '--box-shadow': 'rgba(255,255,255,0.1)'
+}
+
+const customTheme: Theme = () => {
+    cache.getTheme()
+}
+
 // 定义 Pinia Store
 export const useThemeStore = defineStore('theme', {
     state: () => ({
         currentTheme: 'light',  // 默认为亮色主题
-        lightTheme: {
-            '--van-primary-color': '#1989fa',
-            '--van-success-color': '#07c160',
-            '--van-danger-color': '#ee0a24',
-            '--van-warning-color': '#ff976a',
-            '--van-text-color': '#323233',
-            '--van-text-color-2': '#969799',
-            '--van-text-color-3': '#c8c9cc',
-            '--van-active-color': '#f2f3f5',
-            '--van-active-opacity': 0.6,
-            '--van-disabled-opacity': 0.5,
-            '--van-background': '#f7f8fa',
-            '--van-background-2': '#fff',
-            '--van-background-3': '#f1f1f1',
-            '--box-shadow': 'rgba(0, 0, 0, 0.1)',
-        } as Theme,
-        darkTheme: {
-            '--van-primary-color': '#1989fa',
-            '--van-success-color': '#07c160',
-            '--van-danger-color': '#ee0a24',
-            '--van-warning-color': '#ff976a',
-            '--van-text-color': '#f5f5f5',
-            '--van-text-color-2': '#707070',
-            '--van-text-color-3': '#4d4d4d',
-            '--van-active-color': '#3a3a3c',
-            '--van-active-opacity': 0.6,
-            '--van-disabled-opacity': 0.5,
-            '--van-background': '#000',
-            '--van-background-2': '#1c1c1e',
-            '--van-background-3': '#37363b',
-            '--box-shadow': 'rgba(255,255,255,0.1)'
-        } as Theme,
     }),
 
     actions: {
@@ -62,12 +60,12 @@ export const useThemeStore = defineStore('theme', {
             let themeToApply: Theme;
 
             if (theme === 'light') {
-                themeToApply = this.lightTheme;
+                themeToApply = lightTheme;
             } else if (theme === 'dark') {
-                themeToApply = this.darkTheme;
+                themeToApply = darkTheme;
             } else {
-                // 在这里可以使用自定义主题
-                themeToApply = this.lightTheme; // 默认使用 light theme
+                // 使用自定义主题
+                themeToApply = lightTheme;
             }
 
             this.applyTheme(themeToApply);
--- src/utlis/cache.ts
+++ src/utlis/cache.ts
@@ -1,8 +1,10 @@
 import {Storage} from "./storage"
+import {lightTheme, Theme} from "@/store/theme";
 
 const key = {
     refreshTokenKey: 'loveRefreshToken',
     loginUsername: 'loveLoginUsername',
+    theme: 'loveTheme',
 }
 
 // 缓存
@@ -23,6 +25,13 @@ class Cache {
         Storage.setItem(key.loginUsername, value)
     }
 
+    getTheme = (): Theme => {
+        return Storage.getItem(key.theme) ?? lightTheme
+    }
+
+    setTheme = (value: Theme) => {
+        Storage.setItem(key.theme, value)
+    }
 }
 
 export default new Cache()
