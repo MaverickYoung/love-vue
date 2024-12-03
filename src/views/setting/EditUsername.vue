7fca8a3c9ba1f5223729572fd9198b6cd496e5e2
Maverick <2410345576@qq.com>
2024-12-03 16:23:48 +0800

    :necktie: 添加设置相关子路由

---
+++ src/views/setting/EditUsername.vue
@@ -0,0 +1,24 @@
+<template>
+  <form class="edit-field-form">
+    <label>用户名
+      <input
+          v-model="username"
+          type="text"
+          placeholder="用户名 2-20位 不含空格"
+      />
+    </label>
+    <p v-if="error" class="error">{{ error }}</p>
+    <van-button>保存</van-button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+const username = ref<string>('');
+const error = ref<string | null>(null);
+</script>
+
+<style scoped>
+
+</style>
