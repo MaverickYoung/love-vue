37a0deeffc2ead632f7a47c0f481082684c8f56a
Maverick <2410345576@qq.com>
2024-12-17 11:01:45 +0800

    :sparkles: 图标组件和选择按钮组件

---
+++ src/components/lo-icon/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="icon" class="love-icon">
+    <svg :style="svgStyle" aria-hidden="true">
+      <use :xlink:href="iconName"/>
+    </svg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {computed} from 'vue'
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  color: {
+    type: String,
+    default: ''
+  },
+  size: {
+    type: String,
+    default: ''
+  }
+})
+
+const iconName = computed(() => `#icon-${props.icon}`)
+const svgStyle = computed(() => ({
+  color: props.color,
+  width: props.size,
+  height: props.size,
+}));
+</script>
+
+<style scoped>
+.love-icon {
+  display: inline-block;
+
+  svg {
+    width: 1rem;
+    height: 1rem;
+    vertical-align: -0.15rem;
+    fill: currentColor;
+    overflow: hidden;
+    flex-shrink: 0;
+  }
+}
+</style>
