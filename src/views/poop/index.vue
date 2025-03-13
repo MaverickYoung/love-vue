5264cb2fd91998064bcbc4ec11beeee8d9c97442
Fmy0727 <2410345576@qq.com>
2025-03-14 01:10:23 +0800

    :zap: 优化便便页面加载体验

--- src/views/poop/index.vue
+++ src/views/poop/index.vue
@@ -10,19 +10,27 @@
           <template #title>
             <image-wrapper :src="PoopNormalIcon" alt="小便便图标" width="50%" />
           </template>
-          <poop-message />
+          <keep-alive>
+            <poop-message v-if="loadedTabs.includes(0) || activeTab === 0" />
+          </keep-alive>
         </van-tab>
+
         <van-tab class="tab-content">
           <template #title>
             <image-wrapper :src="PoopNormalIcon" alt="小便便图标" width="50%" />
           </template>
-          <poop-reward />
+          <keep-alive>
+            <poop-reward v-if="loadedTabs.includes(1) || activeTab === 1" />
+          </keep-alive>
         </van-tab>
+
         <van-tab class="tab-content">
           <template #title>
             <image-wrapper :src="PoopNormalIcon" alt="小便便图标" width="50%" />
           </template>
-          <poop-stats />
+          <keep-alive>
+            <poop-stats v-if="loadedTabs.includes(2) || activeTab === 2" />
+          </keep-alive>
         </van-tab>
       </van-tabs>
     </div>
@@ -31,13 +39,21 @@
 
 <script lang="ts" setup>
 import ImageWrapper from '@/components/ImageWrapper.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import PoopMessage from '@/views/poop/PoopMessage.vue';
 import PoopReward from '@/views/poop/PoopReward.vue';
 import PoopStats from '@/views/poop/PoopStats.vue';
 import { PoopNormalIcon } from '@/assets';
 
-const activeTab = ref('');
+// 默认加载第一个tab
+const activeTab = ref<number>(0);
+const loadedTabs = ref<number[]>([0]);
+watch(activeTab, (newVal) => {
+  // 标记已加载的tab
+  if (!loadedTabs.value.includes(newVal)) {
+    loadedTabs.value.push(newVal);
+  }
+});
 </script>
 
 <style scoped>
